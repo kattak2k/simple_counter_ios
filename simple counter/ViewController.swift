36da3cfeea7779facb2d07fb185e6286b95025bb
Ravi Katta <kkiran.ror@gmail.com>
2019-02-22 22:52:31 -0500

    counter app

--- simple counter/ViewController.swift
+++ simple counter/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var index = 0
+    @IBOutlet weak var totalCountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addToCounter(_ sender: Any) {
+        index += 1
+        totalCountLabel.text = String(index)
+        totalCountLabel.textColor = UIColor.green
+    }
+    
+    @IBAction func clearCount(_ sender: Any) {
+        index = 0
+        totalCountLabel.text = String(index)
+        totalCountLabel.textColor = UIColor.blue
+    }
 }
 
